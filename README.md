2f2ca91e7838c082ea1b7eb7302d4a9da8c042ae
Humberto Bravo <humbandroid@gmail.com>
2020-12-09 09:38:42 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Humbanew UNote App
+
+Um bloco de notas diferenciado.
+
+Disponível Em breve para Mobile/Desktop/Web.
---
+++ hnunoteapp/android/app/src/main/kotlin/com/example/hnunoteapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hnunoteapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
